40e92cb5a249a8b56074550900addecc9088a4d9
walid <82517052+waldoslastosba@users.noreply.github.com>
2024-01-29 01:10:09 +0100

    Update PublicElementsPrinter.java

--- code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
@@ -1,8 +1,13 @@
 package fr.istic.vv;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
+
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Objects;
 
 
 // This class visits a compilation unit and
@@ -19,30 +24,46 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
     public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
         if(!declaration.isPublic()) return;
         System.out.println(declaration.getFullyQualifiedName().orElse("[Anonymous]"));
-        for(MethodDeclaration method : declaration.getMethods()) {
-            method.accept(this, arg);
+        for(FieldDeclaration field : declaration.getFields()){
+            if (!field.isPublic() && !hasGetter(field, declaration)) {
+                writeFieldInfos(field, declaration);
+            }
         }
-        // Printing nested types in the top level
-        for(BodyDeclaration<?> member : declaration.getMembers()) {
-            if (member instanceof TypeDeclaration)
-                member.accept(this, arg);
+    }
+
+    private void writeFieldInfos(FieldDeclaration field, TypeDeclaration<?> classOrInterface) {
+        PackageDeclaration packageDeclaration = classOrInterface.findCompilationUnit().get().getPackageDeclaration().orElse(null);
+        String packageName = Objects.isNull(packageDeclaration) ? "" : packageDeclaration.getNameAsString() + ".";
+
+        try {
+            FileOutputStream outputFile = new FileOutputStream("no_getter_results.txt");
+            PrintWriter pw = new PrintWriter(outputFile);
+
+            pw.println(packageName + classOrInterface.getName() + "." + field.getVariable(0).getNameAsString() + " has no getter");
+
+            pw.close();
+            outputFile.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    private boolean hasGetter(FieldDeclaration field, TypeDeclaration<?> classOrInterface) {
+        String fieldName = field.getVariable(0).getNameAsString();
+        String getterFieldName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+
+        for (MethodDeclaration method : classOrInterface.getMethods()) {
+            if (method.isPublic() && method.getName().asString().equals(getterFieldName)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Override
     public void visit(ClassOrInterfaceDeclaration declaration, Void arg) {
         visitTypeDeclaration(declaration, arg);
-    }
-
-    @Override
-    public void visit(EnumDeclaration declaration, Void arg) {
-        visitTypeDeclaration(declaration, arg);
-    }
-
-    @Override
-    public void visit(MethodDeclaration declaration, Void arg) {
-        if(!declaration.isPublic()) return;
-        System.out.println("  " + declaration.getDeclarationAsString(true, true));
     }
 
 }
